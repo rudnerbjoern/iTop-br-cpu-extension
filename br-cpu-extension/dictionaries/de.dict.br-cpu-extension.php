899317af2810ffd7376b86f313ad7f5053b716e7
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-09-11 12:24:16 +0200

    enhance compatibility to iTop 3.1

--- br-cpu-extension/dictionaries/de.dict.br-cpu-extension.php
+++ br-cpu-extension/dictionaries/de.dict.br-cpu-extension.php
@@ -3,7 +3,7 @@
 /**
  * @copyright   Copyright (C) 2023 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2023-06-01
+ * @version     2023-09-11
  *
  * Localized data
  */
